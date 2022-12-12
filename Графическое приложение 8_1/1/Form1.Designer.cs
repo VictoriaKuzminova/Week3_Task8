48062c4b0c12451a0c04ffea665601f1475d1d82
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-12 21:52:44 +0300

    Add files via upload

---
+++ Графическое приложение 8_1/1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.res_textBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.start_textBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // res_textBox
+            // 
+            this.res_textBox.Location = new System.Drawing.Point(26, 214);
+            this.res_textBox.Name = "res_textBox";
+            this.res_textBox.Size = new System.Drawing.Size(556, 127);
+            this.res_textBox.TabIndex = 7;
+            this.res_textBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(212, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 59);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Изменить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(22, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите строку:";
+            // 
+            // start_textBox
+            // 
+            this.start_textBox.Location = new System.Drawing.Point(225, 12);
+            this.start_textBox.Name = "start_textBox";
+            this.start_textBox.Size = new System.Drawing.Size(357, 96);
+            this.start_textBox.TabIndex = 4;
+            this.start_textBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 360);
+            this.Controls.Add(this.res_textBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start_textBox);
+            this.Name = "Form1";
+            this.Text = "Удаление русских слов";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox res_textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox start_textBox;
+    }
+}
+
---
+++ Графическое приложение 8_1/1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            res_textBox.Text = "";
+            string str="";
+            //Проверка на пустоту
+            try
+            {
+                str = start_textBox.Text;
+                if (String.IsNullOrWhiteSpace(str))
+                {
+                    MessageBox.Show("Строка пустая");
+                    return;
+                }
+            }
+            catch { }
+            string res = Regex.Replace(str, @"([а-я]+)|([А-Я]+)", "");
+            res_textBox.Text+="Результат: " + res;
+        }
+    }
+}
