c8b1c1dfa0caad61f8cdc090346757db063948c7
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-12 21:52:11 +0300

    Add files via upload

---
+++ Консольное приложение 8_1/1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            //Проверка на пустоту
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите строку:  ");
+                    str = Console.ReadLine();
+                    if (String.IsNullOrWhiteSpace(str))
+                    {
+                        Console.WriteLine("Строка пустая");
+                        continue;
+                    }
+                    break;
+                }
+                catch { }
+            }
+            string res = Regex.Replace(str, @"([а-я]+)|([А-Я]+)", "");
+            Console.WriteLine("Результат:"+res);
+        }
+    }
+}
